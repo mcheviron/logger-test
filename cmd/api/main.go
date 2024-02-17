10ecbafae297df7e430ea274c87aca60e6928d88
mcheviron <mostafaabdelhameed@outlook.com>
2024-02-17 12:10:10 +0100

    Add logging to server initialization

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"logger-test/internal/server"
 )
 
 func main() {
 
-	server := server.NewServer()
-
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(fmt.Sprintf("cannot start server: %s", err))
+	loggerConfig := server.LoggerConfig{
+		LoggerType: "json",
+		Level:      slog.LevelDebug,
+		AddSource:  true,
 	}
+
+	logger, err := server.NewLogger(loggerConfig)
+	if err != nil {
+		panic(fmt.Sprintf("cannot create logger: %s", err))
+	}
+
+	logger.Info("starting server")
+	logger.Debug("debugging server")
+	logger.Warn("warning server")
+	logger.Error("error server")
 }
